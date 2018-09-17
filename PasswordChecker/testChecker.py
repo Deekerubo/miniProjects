053bc1444bd85b0d2cc5920fe7b859d011afd890
Deekerubo <dianakerubo24@gmail.com>
2018-09-17 14:51:57 +0300

    tests for valididty

--- PasswordChecker/testChecker.py
+++ PasswordChecker/testChecker.py
@@ -1,3 +1,14 @@
 import unittest
 from unittest.mock import patch
 import check_password
+
+class ValidPassword(unittest.TestCase):
+    def test_valid_password(self):
+        user_input = ['Kanini24!','shakes123','nyambumo']
+        output = 'Kanini24!'
+        with patch('builtins.input', side_effect=user_input):
+            res = check_password.PasswordValidity()
+        self.assertEqual(res, output)
+
+if __name__ == "__main__":
+    unittest.main()
