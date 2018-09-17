b7fbe883a8b6e0caeb26ef212993a5d218372a15
Deekerubo <dianakerubo24@gmail.com>
2018-09-17 14:33:09 +0300

    Check password Validity

--- PasswordChecker/pchecker.py
+++ PasswordChecker/pchecker.py
@@ -1,2 +1,11 @@
 import re
-def 
+
+def PasswordValidity():
+     passwords = input("Enter passwords separated by a comma: ").split(",")
+     regex = "^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[$#@])[a-zA-Z0-9$#@]{6,12}$"
+     PasswordValidity = []
+     for password in passwords:
+         if re.match(regex, password):
+             PasswordValidity.append(password)
+        return ','.join(PasswordValidity)
+             
